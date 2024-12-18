d9557e724cf63c1e62e92cdf2b6bcac8c65db941
panda44312 <panda44312@gmali.com>
2024-12-18 15:23:17 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,25 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    
+    icon: path.join(__dirname, 'app-icon', 'app-icon.ico'), // 设置窗口图标
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+    }
+  });
+
+  mainWindow.loadURL('https://google.com'); // 加载你的应用
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
